fb507de45b823d9d81fcdc633e6efc5cfc648e00
Mehdi Zerouali <mehdi@sigmaprime.io>
2019-06-12 13:44:02 +1000

    Initial fuzzer for packet decode discv5

--- misc/discv5/fuzz/fuzz_targets/fuzz_target_1.rs
+++ misc/discv5/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,7 +1,13 @@
 #![no_main]
 #[macro_use] extern crate libfuzzer_sys;
 extern crate libp2p_discv5;
+use libp2p_discv5::packet::Packet;
+use simple_logger;
+use log;
 
 fuzz_target!(|data: &[u8]| {
     // fuzzed code goes here
+    let _ = simple_logger::init_with_level(log::Level::Debug);
+    Packet::decode(data,data);
+
 });
