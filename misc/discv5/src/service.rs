db8c17118ffb3bbe3f47450af80ba1bda7fa9b96
pawan <pawandhananjay@gmail.com>
2020-04-18 15:28:26 +0530

    Fix service poll

--- misc/discv5/src/service.rs
+++ misc/discv5/src/service.rs
@@ -21,7 +21,7 @@ pub struct Discv5Service {
     /// The UDP socket for interacting over UDP.
     socket: UdpSocket,
     /// The buffer to accept inbound datagrams.
-    recv_buffer: Box<[u8; MAX_PACKET_SIZE]>,
+    recv_buffer: [u8; MAX_PACKET_SIZE],
     /// List of discv5 packets to send.
     send_queue: Vec<(SocketAddr, Packet)>,
     /// WhoAreYou Magic Value. Used to decode raw WHOAREYOU packets.
@@ -39,7 +39,7 @@ impl Discv5Service {
 
         Ok(Discv5Service {
             socket,
-            recv_buffer: Box::new([0; MAX_PACKET_SIZE]),
+            recv_buffer: [0; MAX_PACKET_SIZE],
             send_queue: Vec::new(),
             whoareyou_magic,
         })
@@ -49,34 +49,26 @@ impl Discv5Service {
     pub fn send(&mut self, to: SocketAddr, packet: Packet) {
         self.send_queue.push((to, packet));
     }
-}
-/// Drive reading/writing to the UDP socket.
-impl Future for Discv5Service {
-    type Output = (SocketAddr, Packet);
-    fn poll(self: Pin<&mut Self>, cx: &mut task::Context<'_>) -> Poll<(SocketAddr, Packet)> {
-        // let service1 = self.clone();
-        let service = self.get_mut();
-        // send messages
-        while !service.send_queue.is_empty() {
-            let (dst, packet) = service.send_queue.remove(0);
 
-            // TODO: seems very hacky! Check if there's a better way
-            let encoded = packet.encode();
-            let future = service.socket.send_to(&encoded, &dst);
-            pin!(future);
-            match future.poll(cx) {
+    /// Drive reading/writing to the UDP socket.
+    pub fn poll(&mut self, cx: &mut task::Context<'_>) -> Poll<(SocketAddr, Packet)> {
+        // send messages
+        while !self.send_queue.is_empty() {
+            let (dst, packet) = self.send_queue.remove(0);
+
+            match self.socket.poll_send_to(cx, &packet.encode(), &dst) {
                 Poll::Ready(Ok(bytes_written)) => {
                     debug_assert_eq!(bytes_written, packet.encode().len());
                 }
                 Poll::Pending => {
                     // didn't write add back and break
-                    service.send_queue.insert(0, (dst, packet));
+                    self.send_queue.insert(0, (dst, packet));
                     // notify to try again
                     cx.waker().wake_by_ref();
                     break;
                 }
                 Poll::Ready(Err(_)) => {
-                    service.send_queue.clear();
+                    self.send_queue.clear();
                     break;
                 }
             }
@@ -84,18 +76,10 @@ impl Future for Discv5Service {
 
         // handle incoming messages
         loop {
-            // TODO: seems very hacky! Check if there's a better way and if its correct
-            let mut recv_buf: Pin<_> = service.recv_buffer.clone().into();
-            let mut recv_buf_mut = *recv_buf.as_mut();
-            let future = service.socket.recv_from(&mut recv_buf_mut);
-
-            pin!(future); // async functions return GenericFuture which are !Unpin
-
-            match future.poll(cx) {
+            match self.socket.poll_recv_from(cx, &mut self.recv_buffer) {
                 Poll::Ready(Ok((length, src))) => {
-                    let whoareyou_magic = service.whoareyou_magic;
-                    let recv_buffer = *recv_buf.as_ref();
-                    match Packet::decode(&recv_buffer[..length], &whoareyou_magic) {
+                    let whoareyou_magic = self.whoareyou_magic;
+                    match Packet::decode(&self.recv_buffer[..length], &whoareyou_magic) {
                         Ok(p) => {
                             return Poll::Ready((src, p));
                         }
